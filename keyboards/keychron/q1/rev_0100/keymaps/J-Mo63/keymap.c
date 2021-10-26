fc801d29da64211a790d7a32f0a989133186bb2a
Jonathan Moallem <jdmoallem@icloud.com>
2021-10-26 17:59:37 +1100

    Fixed arrow key win_cmd actions, added win_cmdtab state, and fixed up key bindings

--- keyboards/keychron/q1/rev_0100/keymaps/J-Mo63/keymap.c
+++ keyboards/keychron/q1/rev_0100/keymaps/J-Mo63/keymap.c
@@ -31,18 +31,73 @@ enum custom_keycodes {
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
-bool process_record_user(uint16_t keycode, keyrecord_t* record) {
-    switch (keycode) {
+void switch_override(keyrecord_t* record, uint16_t from, uint16_t to)
+{
+    // Register and deregister override keys in reverse order
+    if (record->event.pressed)
+    {
+        unregister_code(from);
+        register_code(to);
+    }
+    else
+    {
+        unregister_code(to);
+        register_code(from);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record)
+{
+    // Set state variables
+    static bool win_cmd_active = false;
+    static bool win_cmdtab_active = false;
+    
+    // Exit win_cmdtab state and bail if key is not tab
+    if (win_cmdtab_active && keycode != KC_TAB)
+    {
+        unregister_code(KC_LALT);
+        win_cmdtab_active = false;
+        return false;
+    }
+
+    switch (keycode)
+    {
         case WIN_CMD:
-            if (record->event.pressed) {
-                register_code(KC_LCTL);
-                layer_on(WIN_FN);
-            } else {
-                layer_off(WIN_FN);
+            // Set win_cmd state and register lctrl
+            win_cmd_active = record->event.pressed;
+            if (win_cmd_active) register_code(KC_LCTL);
+            else unregister_code(KC_LCTL);
+            break;
+        case KC_LEFT:
+            if (!win_cmd_active) break;
+            switch_override(record, KC_LCTL, KC_HOME);
+            return false;
+        case KC_RGHT:
+            if (!win_cmd_active) break;
+            switch_override(record, KC_LCTL, KC_END);
+            return false;
+        case KC_UP:
+            if (!win_cmd_active) break;
+            switch_override(record, KC_LCTL, KC_PGUP);
+            return false;
+        case KC_DOWN:
+            if (!win_cmd_active) break;
+            switch_override(record, KC_LCTL, KC_PGDN);
+            return false;
+        case KC_TAB:
+            if (!win_cmd_active) break;
+            // Switch from win_cmd to win_cmdtab state on tab
+            if (record->event.pressed && !win_cmdtab_active)
+            {
                 unregister_code(KC_LCTL);
+                win_cmd_active = false;
+                register_code(KC_LALT);
+                win_cmdtab_active = true;
+                // Allow pass-through for tab
             }
             break;
     }
+
     return true;
 }
 
@@ -70,14 +125,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      KC_NO,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
-     KC_LALT,  KC_LGUI,  WIN_CMD,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LGUI,  KC_LALT,  WIN_CMD,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_ansi_82( 
      KC_TRNS,            KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_PGUP, 
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_HOME,  KC_PGDN,  KC_END)
+     RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS, 
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
 
 };
