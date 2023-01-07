e638cc94fc557422e3690680abec1301f54ea52d
Jonathan Moallem <jdmoallem@icloud.com>
2023-01-07 11:22:50 +1000

    Implemented personal keymap for Keychron V1

--- keyboards/keychron/v1/ansi/keymaps/J-Mo63/keymap.c
+++ keyboards/keychron/v1/ansi/keymaps/J-Mo63/keymap.c
@@ -28,12 +28,200 @@ enum layers{
 #define KC_TASK LGUI(KC_TAB)
 #define KC_FLXP LGUI(KC_E)
 
+enum custom_keycodes{
+    WIN_CMD = SAFE_RANGE,
+    WIN_CTRL,
+    MOUSE
+};
+
+void switch_key(keyrecord_t* record, uint16_t from, uint16_t to)
+{
+    // Register and deregister switched keys in reverse order
+    if (record->event.pressed)
+    {
+        unregister_code(from);
+        register_code(to);
+    }
+    else
+    {
+        unregister_code(to);
+        register_code(from);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record)
+{
+    // Define states for FSM
+    enum key_states {
+        NONE_STATE,
+        WIN_CMD_STATE,
+        WIN_CMDTAB_STATE,
+        WIN_CTRL_STATE,
+        MOUSE_STATE
+    };
+
+    // Set intial state
+    static enum key_states state = NONE_STATE;
+
+    // Define state transitions
+    void enter_state_none(void)
+    {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        state = NONE_STATE;
+    }
+
+    void enter_state_cmd(void)
+    {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LALT);
+        register_code(KC_LCTL);
+        state = WIN_CMD_STATE;
+    }
+
+    void enter_state_cmdtab(void)
+    {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LCTL);
+        register_code(KC_LALT);
+        state = WIN_CMDTAB_STATE;
+    }
+
+    void enter_state_ctrl(void)
+    {
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        register_code(KC_LGUI);
+        state = WIN_CTRL_STATE;
+    }
+
+    void enter_state_mouse(void)
+    {
+        unregister_code(KC_LGUI);
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LALT);
+        state = MOUSE_STATE;
+    }
+
+    // Run state update for key event
+    switch (state)
+    {
+        case NONE_STATE:
+            if (keycode == WIN_CMD && record->event.pressed)
+            {
+                enter_state_cmd();
+            }
+            else if (keycode == WIN_CTRL && record->event.pressed)
+            {
+                enter_state_ctrl();
+            }
+            else if (keycode == MOUSE && record->event.pressed)
+            {
+                enter_state_mouse();
+            }
+            break;
+        case WIN_CMD_STATE:
+            switch (keycode)
+            {
+                case WIN_CMD:
+                    if (!record->event.pressed) enter_state_none();
+                    break;
+                case KC_LEFT:
+                    switch_key(record, KC_LCTL, KC_HOME);
+                    return false;
+                case KC_RGHT:
+                    switch_key(record, KC_LCTL, KC_END);
+                    return false;
+                case KC_UP:
+                    switch_key(record, KC_LCTL, KC_PGUP);
+                    return false;
+                case KC_DOWN:
+                    switch_key(record, KC_LCTL, KC_PGDN);
+                    return false;
+                case KC_TAB:
+                    if (record->event.pressed) enter_state_cmdtab();
+                    // Allow pass-through for tab
+                    break;
+            }
+            break;
+        case WIN_CMDTAB_STATE:
+            if (keycode == WIN_CMD && !record->event.pressed)
+            {
+                enter_state_none();
+            }
+            else if (keycode != KC_TAB)
+            {
+                enter_state_cmd();
+                return false;
+            }
+            break;
+        case WIN_CTRL_STATE:
+            switch (keycode)
+            {
+                case WIN_CTRL:
+                    if (!record->event.pressed) enter_state_none();
+                    break;
+                // Add control key for workspace switch macro
+                case KC_LEFT:
+                case KC_RGHT:
+                    if (record->event.pressed) register_code(KC_LCTL);
+                    else unregister_code(KC_LCTL);
+                    break;
+            }
+            break;
+        case MOUSE_STATE:
+            switch (keycode)
+            {
+                case MOUSE:
+                    if (!record->event.pressed)
+                    {
+                        // Unregister all potential mouse events
+                        unregister_code(KC_MS_LEFT);
+                        unregister_code(KC_MS_RIGHT);
+                        unregister_code(KC_MS_UP);
+                        unregister_code(KC_MS_DOWN);
+                        unregister_code(KC_MS_BTN1);
+                        unregister_code(KC_MS_BTN2);
+                        enter_state_none();
+                    }
+                    break;
+                case KC_LEFT:
+                    if (record->event.pressed) register_code(KC_MS_LEFT);
+                    else unregister_code(KC_MS_LEFT);
+                    return false;
+                case KC_RGHT:
+                    if (record->event.pressed) register_code(KC_MS_RIGHT);
+                    else unregister_code(KC_MS_RIGHT);
+                    return false;
+                case KC_UP:
+                    if (record->event.pressed) register_code(KC_MS_UP);
+                    else unregister_code(KC_MS_UP);
+                    return false;
+                case KC_DOWN:
+                    if (record->event.pressed) register_code(KC_MS_DOWN);
+                    else unregister_code(KC_MS_DOWN);
+                    return false;
+                case KC_RCTL:
+                    if (record->event.pressed) register_code(KC_MS_BTN1);
+                    else unregister_code(KC_MS_BTN1);
+                    return false;
+                case KC_RSFT:
+                    if (record->event.pressed) register_code(KC_MS_BTN2);
+                    else unregister_code(KC_MS_BTN2);
+                    return false;
+            }
+            break;
+    }
+    return true;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_ansi_82(
         KC_ESC,   KC_BRID,  KC_BRIU,  KC_NO,    KC_NO,    RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  KC_DEL,             KC_INS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        MOUSE,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LOPT,  KC_LCMD,                                KC_SPC,                                 KC_RCMD,  MO(MAC_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
@@ -49,9 +237,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,     KC_F12,   KC_DEL,             KC_INS,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
+        MOUSE,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+        WIN_CTRL, KC_LALT,  WIN_CMD,                                KC_SPC,                                 KC_RALT,  MO(WIN_FN), KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_ansi_82(
         _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,    KC_VOLU,  _______,            _______,
